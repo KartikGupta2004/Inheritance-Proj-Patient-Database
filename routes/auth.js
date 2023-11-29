1213d4fe4d58ce6a75b155086f57c2d3d641c759
divyannshu <divyanshum3232@gmail.com>
2023-11-29 23:53:17 +0530

    console logged

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,14 @@ const router=express.Router()
 const User=require('../models/User')
 const { query, validationResult } = require('express-validator');
 //Create a User using:POST "/api/auth/".Doesnt require auth
-router.get('/',[
+router.post('/',[
     query('name').isLength({min:5})
 ],(req,res)=>{
     console.log(req.body);
     const user=User(req.body)
     user.save()
     res.send(req.body)
+    console.log('Hello world');
 
 })
 module.exports=router
