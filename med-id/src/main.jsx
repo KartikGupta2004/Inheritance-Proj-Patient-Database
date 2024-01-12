108af18be11bdf55d02653f370fc65ddd844c757
gpsgaurav18 <gpsgaurav012@gmail.com>
2024-01-12 12:21:59 +0530

    Added Comment

--- med-id/src/main.jsx
+++ med-id/src/main.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-import Features from './Components/Features.jsx'
+import Features from "./Components/Features.jsx";
 import RecordForm from "./Components/RecordForm.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Features/>
+    <Features />
+    {/* <RecordForm/> */}
   </React.StrictMode>
 );
