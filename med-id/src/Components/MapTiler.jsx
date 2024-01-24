a3cb06685c25a377b2a94a990cd483a3a73a1f7c
gpsgaurav18 <gpsgaurav012@gmail.com>
2024-01-25 01:19:36 +0530

    Sec Changes

--- med-id/src/Components/MapTiler.jsx
+++ med-id/src/Components/MapTiler.jsx
@@ -1,5 +1,7 @@
+const MY_API_KEY = import.meta.env.VITE_MAPTILER_API_KEY;
+
 const maptiler = {
-  url: "https://api.maptiler.com/maps/bright-v2/{z}/{x}/{y}@2x.png?key=1RsBaBSNZIk8l8J6MziN",
+  url: `https://api.maptiler.com/maps/bright-v2/{z}/{x}/{y}@2x.png?key=${MY_API_KEY}`,
   attribution:
     '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
 };
