ba197e6ebe6188610806619a5985362ede0bc62c
divyannshu <divyanshum3232@gmail.com>
2024-02-04 19:45:40 +0530

    Cors added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const connectToMongo=require('./db')
 const express = require('express')
 const morgan=require('morgan')
+const cors=require('cors')
 const app = express()
 const port = 5000
 app.use(morgan('combined'))
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth',require('./routes/auth'))
