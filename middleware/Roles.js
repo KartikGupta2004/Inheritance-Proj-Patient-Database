daccd2feb703711cca94f99813b1163bb3382478
divyannshu <divyanshum3232@gmail.com>
2024-02-10 22:47:08 +0530

    Restoring

--- middleware/Roles.js
+++
@@ -1,9 +0,0 @@
-// // const passport=require('passport')
-// //Middle ware to check if the user has the required roles
-// const checkUserRole=(requiredRoles)=>(req,res,next)=>{
-//     if(req.user && requiredRoles.includes(req.user.role)){
-//         return next()
-//     }else
-//     return res.status(403).json({messsage:'Access denied.Insufficient permissions.'})
-// }
-// module.exports=checkUserRole
